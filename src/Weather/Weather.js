26ad3158638e837d2055de0486f144c905a7c81e
Anibe Agamah <mail@anibeagamah.com>
2019-01-17 12:51:05 +0000

    Releases *some* of the new SVG icons

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -85,19 +85,19 @@ class Weather extends Component {
     mapIcons(apiIconLabel) {
         // http://unicode.org/emoji/charts/full-emoji-list.html
         let emojiIconTable = {
-                'partlycloudy': 'partlycloudy.svg',//'U+1F325',
-                'chancerain':'chancerain.svg',
-                'storm':'storm.svg',
-                'chancetstorms':'chancestorms.svg',
-                'tstorms':'tstroms.svg',
-                'partlysunny': this.state.timeOfDay === 'day' ? 'partlycloudy.svg':'halfmoon.svg',
-                'sunny': this.state.timeOfDay === 'day' ? 'sunny.svg':'fullmoon.svg',
-                'clear':this.state.timeOfDay === 'day' ? 'sunny.svg':'fullmoon.svg',
-                'snow':'snow.svg',
-                'rain':'rain.svg',
-                'mostlycloudy':'mostlycloudy.svg',
-                'cloudy':'cloudy.svg',
-                'fog':'fog.svg'
+                'partlycloudy': 'partlycloudy',//'U+1F325',
+                'chancerain':'chancerain',
+                'storm':'storm',
+                'chancetstorms':'chancestorms',
+                'tstorms':'tstroms',
+                'partlysunny': this.state.timeOfDay === 'day' ? 'partlycloudy':'halfmoon',
+                'sunny': this.state.timeOfDay === 'day' ? 'sunny':'fullmoon',
+                'clear':this.state.timeOfDay === 'day' ? 'sunny':'fullmoon',
+                'snow':'snow',
+                'rain':'rain',
+                'mostlycloudy':'cloudy',
+                'cloudy':'cloudy',
+                'fog':'fog'
             };
 
         return emojiIconTable[apiIconLabel];
@@ -120,17 +120,21 @@ class Weather extends Component {
                 <div className="main-title">
                     {this.state.forecast.today[0]}&deg;
                 </div>
-                <div className="no-font icon"><img src={this.mapIcons(this.state.forecast.today[1])} width="40" height="40" /></div>
+                <div className={`no-font icon ${this.mapIcons(this.state.forecast.today[1])}`}>&nbsp;</div>
                 <div className="other-days">
                     <div className="tomorrow">
                         <div className="heading">Tomorrow</div>
-                        {this.state.forecast.tomorrow[0]}&deg;
-                        <div className="small-icon"><img src={this.mapIcons(this.state.forecast.tomorrow[1])} width="40" height="40" /></div>
+                        <div className="data">
+                            <div className="temp">{this.state.forecast.tomorrow[0]}&deg;</div>
+                            <div className={`small-icon ${this.mapIcons(this.state.forecast.tomorrow[1])}`}>&nbsp;</div>
+                        </div>
                     </div>
                     <div className="nexttomorrow">
                         <div className="heading">{this.twoDaysFromToday()}</div>
-                        {this.state.forecast.nexttomorrow[0]}&deg;
-                        <div className="small-icon"><img src={this.state.forecast.nexttomorrow[1]} width="40" height="40" /></div>
+                        <div className="data">
+                            <div className="temp">{this.state.forecast.nexttomorrow[0]}&deg;</div>
+                            <div className={`small-icon ${this.mapIcons(this.state.forecast.nexttomorrow[1])}`}>&nbsp;</div>
+                        </div>
                     </div>
                 </div>
             </div>
