704cbe55d99c1f571e8c08c51176c73b337c2e22
Anibe Agamah <mail@anibeagamah.com>
2019-11-01 16:35:33 +0000

    Extract Trade into a new screen

--- src/App.js
+++ src/App.js
@@ -27,7 +27,72 @@ class App extends Component {
         calendarId: 'uhdj9bf33sav2qqml8gii7l2nutab9l3@import.calendar.google.com',
         maxResults: 4,
         backgroundColor: '#a8312d'
-      }      
+      },
+      'Trade': {
+        title: 'Trade',
+        currency: 'GBP',
+        apikey: props.apikey,
+        coins: [{
+            "id": "bitcoin",
+            "symbol": "BTC",
+            "buy": 7800,
+            "sell": 24000
+        },
+        {
+            "id":"ethereum",
+            "symbol": "ETH",
+            "buy": 780,
+            "sell": 1900    
+        },
+        {
+            "id": "ripple",
+            "symbol": "XRP",
+            "buy": 0.9,
+            "sell": 4    
+        },        
+        {
+            "id": "neo",
+            "symbol": "NEO",
+            "buy": 100,
+            "sell": 300    
+        },
+        {
+            "id": "vechain",
+            "symbol": "VEN",
+            "buy": 4.6,
+            "sell": 17    
+        },                
+        {
+            "id": "omisego",
+            "symbol": "OMG",
+            "buy": 10.5,
+            "sell": 40
+        },
+        {
+            "id": "stratis",
+            "symbol": "STRAT",
+            "buy": 8,
+            "sell": 32  
+        },        
+        {
+            "id": "stellar",
+            "symbol": "XLM",
+            "buy": 0.4,
+            "sell": 1.2  
+        },
+        {
+            "id": "qash",
+            "symbol": "QASH",
+            "buy": 0.8,
+            "sell": 2.4  
+        },
+        {
+            "id": "ignis",
+            "symbol": "IGNIS",
+            "buy": 0.4,
+            "sell": 1.6            
+        }]
+    }    
     }
   }
 
@@ -62,6 +127,7 @@ class App extends Component {
         <Calendar settings={this.settings.Events} />
         <Calendar settings={this.settings.Arsenal} />
         <Coin />
+        <Trade settings={this.settings.Trade} />
         <Clock format={ settings.Clock.is24HourFormat } colour={settings.Clock.colour} />
       </div>
     );
