77ba8cc51074c83972824bb1998fee2569f5338f
Anibe Agamah <mail@anibeagamah.com>
2020-11-22 17:09:39 +0000

    Remove Coin

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import config from './config.json';
 import Clock from './Clock/Clock';
 import Calendar from './Calendar/Calendar';
 import Weather from './Weather/Weather';
-// import Trade from './Trade/Trade';
-import Coin from './Coin/Coin';
+import Trade from './Trade/Trade';
+// import Coin from './Coin/Coin';
 import './App.css';
 
 class App extends Component {
@@ -86,8 +86,8 @@ class App extends Component {
         <Weather apikey={config.api.weatherunderground.key} />
         <Calendar settings={this.settings.Events} />
         <Calendar settings={this.settings.Arsenal} />
-        {/* <Trade settings={this.settings.Trade} /> */}
-        <Coin />
+        <Trade settings={this.settings.Trade} />
+        {/* <Coin /> */}
         <Clock format={ settings.Clock.is24HourFormat } colour={settings.Clock.colour} />
       </div>
     );
